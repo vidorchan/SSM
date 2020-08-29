a9c8cf6026251f36eb77ca1d110bbddac221af63
vidorchan <1282911204@qq.com>
2020-08-29 22:39:31 +0800

    spring+spring mvc+maven

--- src/main/java/com/vidor/controller/HelloController.java
+++ src/main/java/com/vidor/controller/HelloController.java
@@ -1,15 +1,8 @@
 package com.vidor.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class HelloController {
